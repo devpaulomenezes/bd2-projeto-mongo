44420e0d15447a7ff199d2e86197313c5cbd4559
Paulo Moura Menezes <devpaulomenezes@gmail.com>
2026-08-29 04:14:21 -0300

    feat: initial commit - Spring Boot com MongoDB para BD2

---
+++ src/main/java/br/edu/ifpb/es/bd2/Bd2MongodbApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.es.bd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bd2MongodbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bd2MongodbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/es/bd2/Daw2MongodbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.es.bd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daw2MongodbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
